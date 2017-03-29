679438f6f882116118381532a5114e65e540cb03
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2017-03-29 18:43:28 -0500

    updating stuff

--- src/main/java/com/gmke/stateless/auth/config/ResourceServerConfiguration.java
+++ src/main/java/com/gmke/stateless/auth/config/ResourceServerConfiguration.java
@@ -1,7 +1,10 @@
 package com.gmke.stateless.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -9,8 +12,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+    @Autowired
+    private ClientResources clientResources;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/profile").authorizeRequests().anyRequest().authenticated();
+        http.antMatcher("/scopes").authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    public OAuth2RestTemplate restTemplate() {
+        return new OAuth2RestTemplate(clientResources.getClient());
     }
 }
--- src/main/java/com/gmke/stateless/auth/web/UserResource.java
+++ src/main/java/com/gmke/stateless/auth/web/UserResource.java
@@ -1,14 +1,24 @@
 package com.gmke.stateless.auth.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class UserResource {
+
+    private final OAuth2RestTemplate restTemplate;
+
+    @Autowired
+    public UserResource(OAuth2RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @RequestMapping("/profile")
     public Map<String, String> user(Principal principal) {
@@ -16,6 +26,11 @@ public class UserResource {
         map.put("name", principal.getName());
 
         return map;
+    }
+
+    @RequestMapping("/scopes")
+    public List<String> testStuffs() {
+        return restTemplate.getResource().getScope();
     }
 
 }
